afee8652ba3a718656099284546ed2bf6d11de20
Josh Zutell <joshua.zutell.18@cnu.edu>
2019-06-06 17:05:17 -0400

    Upgraded to melodic

--- pointgrey_camera_driver/src/PointGreyCamera.cpp
+++ pointgrey_camera_driver/src/PointGreyCamera.cpp
@@ -8,7 +8,7 @@ This software is released under a BSD license:
 Copyright (c) 2012, Carnegie Mellon University. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
+roslaunch jackal_viz view_model.launch
 Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 Neither the name of the Carnegie Mellon University nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
